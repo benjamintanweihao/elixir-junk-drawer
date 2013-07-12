ae15f64b446471cc3e7242bd6a17db52617e80a9
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2013-07-12 22:03:20 +0800

    completed another etude

--- stats.exs
+++ stats.exs
@@ -1,4 +1,5 @@
 defmodule Stats do
+
   def minimum([head|tail]) do
     minimum(tail, head)
   end
@@ -32,4 +33,27 @@ defmodule Stats do
   def range(list) do
     [ minimum(list), maximum(list) ]
   end
+
+  # foldl(list, acc, function)
+  def mean(list) do
+    List.foldl(list, 0, fn (x, acc) -> x + acc end) / Enum.count(list)
+  end
+
+  # My noob version.
+  def stdv(list) do
+    sum = List.foldl(list, 0, fn (x, acc) -> x + acc end)
+    sum_of_squares = List.foldl(list, 0, fn (x, acc) -> (x * x) + acc end)
+
+    n = Enum.count(list)
+    :math.sqrt ((n * sum_of_squares - sum * sum) / (n * (n -1)))
+  end
+
+  # Holy shit ... this is awesome. Process 2 lists at once!
+  def stdv(list) do
+    n = Enum.count(list)
+    {sum, sum_of_squares} = List.foldl(list, {0,0}, fn(x, {acc, acc_squares}) -> 
+      {x + acc, x*x + acc_squares} end)
+    :math.sqrt ((n * sum_of_squares - sum * sum) / (n * (n -1)))
+  end
+
 end
